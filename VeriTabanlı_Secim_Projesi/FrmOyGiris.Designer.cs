2bd2bec08f21abe5e4085d39ba7f9009e7c53371
-.Ali TAN.- <-.Ali TAN.-@DESKTOP-SJUQRF9>
2023-05-12 20:29:00 +0300

    Add project files.

---
+++ VeriTabanlı_Secim_Projesi/FrmOyGiris.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace VeriTabanlı_Secim_Projesi
+{
+    partial class FrmOyGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblilceAd = new System.Windows.Forms.Label();
+            this.TxtilceAd = new System.Windows.Forms.TextBox();
+            this.LblA = new System.Windows.Forms.Label();
+            this.LblB = new System.Windows.Forms.Label();
+            this.LblC = new System.Windows.Forms.Label();
+            this.LblD = new System.Windows.Forms.Label();
+            this.TxtA = new System.Windows.Forms.TextBox();
+            this.TxtB = new System.Windows.Forms.TextBox();
+            this.TxtC = new System.Windows.Forms.TextBox();
+            this.TxtD = new System.Windows.Forms.TextBox();
+            this.btnOyGiris = new System.Windows.Forms.Button();
+            this.BtnGrafikler = new System.Windows.Forms.Button();
+            this.BtnCikisYap = new System.Windows.Forms.Button();
+            this.TxtE = new System.Windows.Forms.TextBox();
+            this.LblE = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblilceAd
+            // 
+            this.LblilceAd.AutoSize = true;
+            this.LblilceAd.Location = new System.Drawing.Point(13, 20);
+            this.LblilceAd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblilceAd.Name = "LblilceAd";
+            this.LblilceAd.Size = new System.Drawing.Size(58, 17);
+            this.LblilceAd.TabIndex = 0;
+            this.LblilceAd.Text = "İlçe Ad:";
+            // 
+            // TxtilceAd
+            // 
+            this.TxtilceAd.Location = new System.Drawing.Point(87, 17);
+            this.TxtilceAd.Name = "TxtilceAd";
+            this.TxtilceAd.Size = new System.Drawing.Size(235, 25);
+            this.TxtilceAd.TabIndex = 1;
+            // 
+            // LblA
+            // 
+            this.LblA.AutoSize = true;
+            this.LblA.Location = new System.Drawing.Point(13, 81);
+            this.LblA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblA.Name = "LblA";
+            this.LblA.Size = new System.Drawing.Size(57, 17);
+            this.LblA.TabIndex = 2;
+            this.LblA.Text = "A Parti:";
+            // 
+            // LblB
+            // 
+            this.LblB.AutoSize = true;
+            this.LblB.Location = new System.Drawing.Point(13, 127);
+            this.LblB.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblB.Name = "LblB";
+            this.LblB.Size = new System.Drawing.Size(56, 17);
+            this.LblB.TabIndex = 3;
+            this.LblB.Text = "B Parti:";
+            // 
+            // LblC
+            // 
+            this.LblC.AutoSize = true;
+            this.LblC.Location = new System.Drawing.Point(13, 173);
+            this.LblC.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblC.Name = "LblC";
+            this.LblC.Size = new System.Drawing.Size(57, 17);
+            this.LblC.TabIndex = 4;
+            this.LblC.Text = "C Parti:";
+            // 
+            // LblD
+            // 
+            this.LblD.AutoSize = true;
+            this.LblD.Location = new System.Drawing.Point(13, 213);
+            this.LblD.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblD.Name = "LblD";
+            this.LblD.Size = new System.Drawing.Size(58, 17);
+            this.LblD.TabIndex = 5;
+            this.LblD.Text = "D Parti:";
+            // 
+            // TxtA
+            // 
+            this.TxtA.Location = new System.Drawing.Point(87, 81);
+            this.TxtA.Name = "TxtA";
+            this.TxtA.Size = new System.Drawing.Size(235, 25);
+            this.TxtA.TabIndex = 6;
+            // 
+            // TxtB
+            // 
+            this.TxtB.Location = new System.Drawing.Point(87, 124);
+            this.TxtB.Name = "TxtB";
+            this.TxtB.Size = new System.Drawing.Size(235, 25);
+            this.TxtB.TabIndex = 7;
+            // 
+            // TxtC
+            // 
+            this.TxtC.Location = new System.Drawing.Point(87, 165);
+            this.TxtC.Name = "TxtC";
+            this.TxtC.Size = new System.Drawing.Size(235, 25);
+            this.TxtC.TabIndex = 8;
+            // 
+            // TxtD
+            // 
+            this.TxtD.Location = new System.Drawing.Point(87, 204);
+            this.TxtD.Name = "TxtD";
+            this.TxtD.Size = new System.Drawing.Size(235, 25);
+            this.TxtD.TabIndex = 9;
+            // 
+            // btnOyGiris
+            // 
+            this.btnOyGiris.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOyGiris.Location = new System.Drawing.Point(87, 284);
+            this.btnOyGiris.Name = "btnOyGiris";
+            this.btnOyGiris.Size = new System.Drawing.Size(235, 44);
+            this.btnOyGiris.TabIndex = 10;
+            this.btnOyGiris.Text = "OY GİRİŞİ YAP";
+            this.btnOyGiris.UseVisualStyleBackColor = true;
+            this.btnOyGiris.Click += new System.EventHandler(this.btnOyGiris_Click);
+            // 
+            // BtnGrafikler
+            // 
+            this.BtnGrafikler.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGrafikler.Location = new System.Drawing.Point(87, 334);
+            this.BtnGrafikler.Name = "BtnGrafikler";
+            this.BtnGrafikler.Size = new System.Drawing.Size(106, 47);
+            this.BtnGrafikler.TabIndex = 11;
+            this.BtnGrafikler.Text = "GRAFİKLER";
+            this.BtnGrafikler.UseVisualStyleBackColor = true;
+            this.BtnGrafikler.Click += new System.EventHandler(this.BtnGrafikler_Click);
+            // 
+            // BtnCikisYap
+            // 
+            this.BtnCikisYap.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCikisYap.Location = new System.Drawing.Point(199, 334);
+            this.BtnCikisYap.Name = "BtnCikisYap";
+            this.BtnCikisYap.Size = new System.Drawing.Size(123, 47);
+            this.BtnCikisYap.TabIndex = 12;
+            this.BtnCikisYap.Text = "ÇIKIŞ YAP";
+            this.BtnCikisYap.UseVisualStyleBackColor = true;
+            // 
+            // TxtE
+            // 
+            this.TxtE.Location = new System.Drawing.Point(87, 244);
+            this.TxtE.Name = "TxtE";
+            this.TxtE.Size = new System.Drawing.Size(235, 25);
+            this.TxtE.TabIndex = 14;
+            // 
+            // LblE
+            // 
+            this.LblE.AutoSize = true;
+            this.LblE.Location = new System.Drawing.Point(13, 253);
+            this.LblE.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblE.Name = "LblE";
+            this.LblE.Size = new System.Drawing.Size(56, 17);
+            this.LblE.TabIndex = 13;
+            this.LblE.Text = "E Parti:";
+            // 
+            // FrmOyGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(373, 399);
+            this.Controls.Add(this.TxtE);
+            this.Controls.Add(this.LblE);
+            this.Controls.Add(this.BtnCikisYap);
+            this.Controls.Add(this.BtnGrafikler);
+            this.Controls.Add(this.btnOyGiris);
+            this.Controls.Add(this.TxtD);
+            this.Controls.Add(this.TxtC);
+            this.Controls.Add(this.TxtB);
+            this.Controls.Add(this.TxtA);
+            this.Controls.Add(this.LblD);
+            this.Controls.Add(this.LblC);
+            this.Controls.Add(this.LblB);
+            this.Controls.Add(this.LblA);
+            this.Controls.Add(this.TxtilceAd);
+            this.Controls.Add(this.LblilceAd);
+            this.Font = new System.Drawing.Font("Segoe UI Symbol", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmOyGiris";
+            this.Text = "Veri Tabanlı Grafik Seçim Sistemi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblilceAd;
+        private System.Windows.Forms.TextBox TxtilceAd;
+        private System.Windows.Forms.Label LblA;
+        private System.Windows.Forms.Label LblB;
+        private System.Windows.Forms.Label LblC;
+        private System.Windows.Forms.Label LblD;
+        private System.Windows.Forms.TextBox TxtA;
+        private System.Windows.Forms.TextBox TxtB;
+        private System.Windows.Forms.TextBox TxtC;
+        private System.Windows.Forms.TextBox TxtD;
+        private System.Windows.Forms.Button btnOyGiris;
+        private System.Windows.Forms.Button BtnGrafikler;
+        private System.Windows.Forms.Button BtnCikisYap;
+        private System.Windows.Forms.TextBox TxtE;
+        private System.Windows.Forms.Label LblE;
+    }
+}
+
